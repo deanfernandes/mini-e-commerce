b5eb245dc06ee652759d4e5371a28eaf166ab494
dean <deanfernandes18@gmail.com>
2025-07-01 14:20:47 +0100

    Add navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,40 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 function Header() {
     return (
         <header>
-            <h1>mini e-commerce</h1>
-            <Link to="/" className="btn btn-primary btn-lg">Home</Link>
-            <Link to="/products" className="btn btn-primary btn-lg">Products</Link>
+            <nav className="navbar bg-primary navbar-expand-lg">
+                <div className="container-fluid">
+                    <h1 className="navbar-brand">mini e-commerce</h1>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink
+                                to="/"
+                                className={({ isActive }) =>
+                                    isActive ? 'nav-link active fw-bold text-decoration-underline' : 'nav-link'
+                                }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                to="/products"
+                                className={({ isActive }) =>
+                                    isActive ? 'nav-link active fw-bold text-decoration-underline' : 'nav-link'
+                                }
+                                >
+                                    Products
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
     );
 }
